273b664008762e79a2cb4c4597a20abb480c07e0
Juan Rodriguez <sjdonado@uninorte.edu.co>
2023-08-20 11:29:55 +0200

    feat: database setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,37 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module, OnModuleInit } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { EntityManager } from 'typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'sqlite',
+        database: configService.get('DATABASE_PATH'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements OnModuleInit {
+  private readonly logger = new Logger(AppModule.name);
+
+  constructor(private readonly entityManager: EntityManager) { }
+
+  async onModuleInit() {
+    await this.entityManager.query('PRAGMA journal_mode = WAL;');
+
+    this.logger.log('Database is in WAL mode.');
+  }
+}
